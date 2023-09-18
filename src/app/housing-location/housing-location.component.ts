0ab2d20864ee5f9ba113ca56f3663d797c06980a
Robb Nicholson <robb.nicholson@gm.com>
2023-09-18 07:52:08 -0700

    lesson 5 complete

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -13,5 +14,11 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+	/* 
+	 The exclamation point is called the non-null assertion operator and it tells the TypeScript 
+	 compiler that the value of this property won't be null or undefined.
+	 https://angular.io/tutorial/first-app/first-app-lesson-05
+	*/
+	@Input() housingLocation!: HousingLocation;
+	
 }
